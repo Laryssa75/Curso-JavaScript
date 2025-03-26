7d3f65e697c3ea23619b7423b0e367724f639946
unknown <laryssamc.ted@gmail.com>
2025-03-26 13:26:23 -0300

    condicional js

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+//console.log('O console funcionou corretamente!')
+
+//condição simples
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
