8101bcd3f15efdc2c13b5e9c96df74e40a0bdf56
unknown <laryssamc.ted@gmail.com>
2025-04-07 10:24:37 -0300

    exercícios com condicional

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,8 @@
+function verificar() {
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementsBId('txtano')
+   var res = document.querySelector('div#res')
+
+   if(fano.value.length == 0 ||)
+}
