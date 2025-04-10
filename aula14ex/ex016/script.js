26e44df99382eb848cc9f30e55577c003e323268
unknown <laryssamc.ted@gmail.com>
2025-04-10 13:42:26 -0300

    exercício de tabuada e de contador finalizados

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,25 +1,40 @@
 function contar() {
-    var inicio = document.getElementById('inicioNum').value
-    var fim = document.getElementById('fimNum').value
-    var passo = document.getElementById('passoNum').value
+    var inicio = document.getElementById('inicioNum')
+    var fim = document.getElementById('fimNum')
+    var passo = document.getElementById('passoNum')
     var resultado = document.getElementById('resultado')
 
     //convertendo para número
-    inicio = Number(inicio)
-    fim = Number(fim)
-    passo = Number(passo)
+    inicio = Number(inicio.value)
+    fim = Number(fim.value)
+    passo = Number(passo.value)
 
-    if(inicio === 0 || fim === 0 || passo === 0){
-        alert('Insira um número maior do que zero!')
+    if(inicio.value.length === 0 || fim.value.length === 0 || passo.value.length === 0){
+        window.alert('Insira um número maior do que zero!')
+        resultado.innerHTML = "Impossível contar!"
+
+    if(passo = 0){
+        window.alert('Passo inválido!Considerando PASSO 1')
+        passo = 1
+    }
     }else {
         resultado.innerHTML = 'Contando:<br>'
         if(inicio < fim) {
-            for(let i = inicio; i < fim; i += passo){
+            for(let i = inicio; i <= fim; i += passo){
                 
                 if(i + passo > fim) {
                     resultado.innerHTML += `${i}` 
                 }else {
                     resultado.innerHTML += `${i} \u{1F449}`
+                }
+            }
+        }else {
+            for(let i = inicio; i >= fim; i -= passo){
+
+                if(i + passo < fim){
+                    resultado.innerHTML -= `${i}`
+                }else {
+                    resultado.innerHTML -= `${i} \u{1F449}`
                 }
             }
         }
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,21 +1,47 @@
-function carregar() {
-    var msg = window.document.querySelector('#msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    //var hora = 19
-    msg.innerHTML = `Agora são ${hora} horas`
-    if(hora >= 0 && hora < 12) {
-        //Bom dia
-        img.src = 'fotomanha.png'
-        document.body.style.background = '#ef891aff'
-    }else if (hora >=12 && hora < 18) {
-        //Boa tarde
-        img.src = 'fototarde.png'
-        document.body.style.background = '#b48575ff'
-    }else {
-        //Boa noite
-        img.src = 'fotonoite.png'
-        document.body.style.background = '#7e6aa7ff'
+function gerarTabuada(){
+    let numero = document.getElementById('num')
+    let resTabuada = document.getElementById('resultadoTabuada')
+
+    
+    //convertendo o input para número
+    if(numero.value.lenght === 0){
+        window.alert('Insira um número maior ou menor do que zero!')
+        
+    }else{
+        let num = Number(numero.value)
+        
+        //limpa a lista antes de criar uma nova
+        resTabuada.innerHTML = ''
+
+       //lista no javascript que será mostrada no html
+        for(let i = 1; i <= 10; i++){
+            // let item = document.createElement('li')
+            let res = num * i
+
+            let item = document.createElement('option')
+            item.text = `${i} x ${num} = ${res}`
+            item.value = `resTabuada${i}`
+            resTabuada.appendChild(item)
+        } 
+
+        // for(let i = 1; i <= 10; i++){
+        //     // let item = document.createElement('li')
+        //     let res = num * i
+
+        //     let input = document.createElement('input')
+        //     input.type = 'text'
+        //     input.value = `${i} x ${num} = ${res}`
+        //     input.readOnly = true
+        //     input.style.margin = '5px 0'
+        //     resTabuada.appendChild(input)
+        // } 
+
+        // for(let i = 1; i <= 10; i++){
+        //     let item = document.createElement('li')
+        //     let res = num * i
+        //     item.textContent = `${i} x ${num} = ${res}`
+        //     resTabuada.appendChild(item)
+        // }
     }
+    
 }
